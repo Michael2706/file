38540026616517fa5d3115ce02c73202e511fcbb
Michael <m.quaschny@gmx.net>
2020-03-10 10:56:43 +0100

    Initiale Commit

---
+++ file/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream stream = File.Open(@"C:\temp\testfile.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            Console.ReadKey();
+
+        }
+    }
+}
